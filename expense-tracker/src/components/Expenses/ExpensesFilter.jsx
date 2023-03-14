ae4c2e399e748471eb5c4a3bca379105739d0697
Karen <karencinca@hotmail.com>
2023-03-14 17:25:48 -0300

    filter by year

---
+++ expense-tracker/src/components/Expenses/ExpensesFilter.jsx
@@ -0,0 +1,24 @@
+import './ExpensesFilter.css';
+
+export const ExpensesFilter = (props) => {
+    const dropYearHandler = (event) => {
+        const yearData = event.target.value
+        props.onShowYearHandler(yearData)
+    }
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropYearHandler}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
